4caa973661a45968d4c40b77f442f4943ea98594
Kimnaram <root@ip-172-31-25-176.ec2.internal>
2020-12-25 15:46:18 +0000

    item 리스트 php 생성_20/12/25

--- ewoman-php/dbcon.php
+++ ewoman-php/dbcon.php
@@ -1,8 +1,8 @@
 <?php
 
-    $host = '';
+    $host = '34.228.20.230';
     $username = 'ewoman'; # MySQL 계정 아이디
-    $password = ''; # MySQL 계정 패스워드
+    $password = 'ewoman'; # MySQL 계정 패스워드
     $dbname = 'ewoman';  # DATABASE 이름
 
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
